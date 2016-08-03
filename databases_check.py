397f095a15100cd71653abeb8195e8ed639875aa
eljayan <eljayan@gmail.com>
2016-08-03 07:55:46 -0500

    trying to create db

--- databases_check.py
+++ databases_check.py
@@ -1,17 +1,36 @@
 #checks existence of dbs or creates them
+import sqlite3
 
 def check(database):
+    cursor = database.cursor()
     #suppliers
 
     #invoices
 
     #cathegories
 
-def create_suppliers(database):
+    return
+
+def create_suppliers(cursor):
     #crea el template de los suppliers
+    return
 
-def create_invoices(database):
+def create_invoices(cursor):
     #crea el template de las facturas
+    return
 
-def create_cathegories(database):
+def create_cathegories(cursor):
     #crea el template de la categoria de expense
+    return
+
+def exists_table(db, t_name):
+    cursor = db.cursor()
+    table_count = cursor.execute("SELECT count(*) FROM {dbn} WHERE type = 'table' AND name = {tn}"\
+                                 .format(dbn = db, tn = t_name))
+    return table_count
+
+if __name__ == '__main__':
+    conn = sqlite3.Connection("test.db")
+    cursor = conn.cursor()
+    #print cursor.execute("")
+    print exists_table(conn, "PROVEEDORES")

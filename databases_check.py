22e16899b305af691a54951c558caf508b0f37b5
Ruben torres <R00715649@CHINA.HUAWEI.COM>
2016-08-10 14:18:14 -0500

    created all the functions to check/create tables

--- databases_check.py
+++ databases_check.py
@@ -1,4 +1,5 @@
-#checks existence of dbs or creates them
+'''checks existence of dbs, tables or creates them'''
+
 import sqlite3
 
 def check(database):
@@ -11,20 +12,34 @@ def check(database):
 
     return
 
-def create_suppliers(cursor):
-    #crea el template de los suppliers
+def create_suppliers(conn):
+    '''crea el template de los suppliers, si no existe'''
+    
+    if exists_table(conn, "PROVEEDORES"):
+        return
+    else:
+        conn.execute("create table PROVEEDORES (ID integer primary key autoincrement, RUC integer(13), NOMBRE text(255))")
     return
 
-def create_invoices(cursor):
-    #crea el template de las facturas
+def create_invoices(conn):
+    '''crea el template de las facturas, si no existe'''
+    if exists_table(conn, "FACTURAS"):
+        return
+    else:
+        conn.execute("create table FACTURAS (ID integer primary key autoincrement, NUMERO text, PROVEEDOR integer, foreign key (PROVEEDOR) references PROVEEDORES(ID))")
     return
 
-def create_cathegories(cursor):
+def create_cathegories(conn):
+    '''crea la tabla de categorias, si no existe'''
+    if exists_table(conn, "CATEGORIAS"):
+        return
+    else:
+        conn.execute("create table CATEGORIAS(ID integer primary key autoincrement, CATEGORIA text, unique(CATEGORIA))")
     #crea el template de la categoria de expense
     return
 
-def exists_table(db, t_name):
-    cursor = db.cursor()
+def exists_table(conn, t_name):
+    cursor = conn.cursor()
     # table_count = cursor.execute("select count(*) from sqlite_master where type = 'table' and name = {tn}"\
     #                              .format(tn = t_name))
 
@@ -33,10 +48,18 @@ def exists_table(db, t_name):
 
 
 if __name__ == '__main__':
-    conn = sqlite3.Connection("test.db")
-    cursor = conn.cursor()
+    required_tables = ["PROVEEDORES", "FACTURAS", "CATEGORIAS"]
+    existing_tables = []
+    conn = sqlite3.connect("test.db")
     #print cursor.execute("")
-    if exists_table(conn, "PROVEEDORE"):
-        print "si existe!"
+    if exists_table(conn, "PROVEEDORES"):
+        existing_tables.append("Proveedores")
     else:
         print "no exite"
+        print "creando"
+        create_suppliers(conn)
+
+    if exists_table(conn, "FACTURAS"):
+        print "tabla de facturas existe!"
+    else:
+       create_invoices(conn) 
---
+++ databases_insert.py
@@ -0,0 +1,12 @@
+'''Insert values into tables'''
+
+import sqlite3
+
+def insert_into_suppliers(conn):
+    return
+
+def insert_into_invoices(conn):
+    return
+
+def insert_into_cathegries(conn):
+    return

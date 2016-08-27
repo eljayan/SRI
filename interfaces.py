21543fb80f8e97615c1a6b6637fc9d3c9c2155ae
Mishell Hurtado Maldonado <MisHurtado@Mishells-MacBook-Air.local>
2016-08-27 18:21:44 -0500

    creating the user interface

---
+++ interfaces.py
@@ -0,0 +1,7 @@
+from Tkinter import *
+
+    class SRI():
+        def __init__(self, root):
+	    self.button_facturas = Button(root, text="Facturas")
+	    self.button_proveedores = Button(root, text="Proveedores")
+	    self.button_export = Button(root, text="Export")
